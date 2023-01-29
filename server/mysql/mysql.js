0a8e679eac15f439913e8f8563c5777650ff713f
Inon Shachar <inonsh01@gmail.com>
2023-01-29 12:59:14 +0200

    start

---
+++ server/mysql/mysql.js
@@ -0,0 +1,54 @@
+var mysql = require('mysql');
+var fs = require('fs');
+const password = "inonsh01";
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: password,
+    database: "seatingDB"
+});
+
+// function createDB() {
+//     con.connect(function (err) {
+//         if (err) throw err;
+//         console.log("Connected!");
+//         con.query("CREATE DATABASE seatingDB", function (err, result) {
+//             if (err) throw err;
+//             console.log("Database created");
+//         });
+//     });
+// }
+// function convertToSQL(obj) {
+//     var tableName = obj.tableName;
+//     var columns = Object.entries(obj.columns).map(entry => entry.join(" ")).join(", ");
+//     var foreignKeys = "";
+//     if (obj.foreignKeys) {
+//         foreignKeys = Object.entries(obj.foreignKeys).map(entry => entry.join(" ")).join(", ");
+//     }
+//     console.log(`CREATE TABLE ${tableName} (${columns}${foreignKeys ? ', ' + foreignKeys : ''})`);
+//     return `CREATE TABLE ${tableName} (${columns}${foreignKeys ? ', ' + foreignKeys : ''})`;
+// }
+
+// function createTables() {
+//     con.connect(function (err) {
+//         if (err) throw err;
+
+//         fs.readdir("/home/hilma/Desktop/Hilma's-Projects/projects/finalProject/server/mysql/entities", (err, files) => {
+//             if (err) throw err;
+//             files.forEach(file => {
+//                 let buffer = fs.readFileSync(`/home/hilma/Desktop/Hilma's-Projects/projects/finalProject/server/mysql/entities/${file}`);
+//                 if (err) throw err;
+//                 let data = JSON.parse(buffer.toString());
+//                 var sql = convertToSQL(data);
+//                 con.query(sql, data.columns, function (err, result) {
+//                     if (err) throw err;
+//                     console.log("Table created");
+//                 })
+//             })
+//         })
+//         console.log("Connected!");
+
+//     });
+// }
+// module.exports = sql;
